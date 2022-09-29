fc63ac056f73478937733811889595bbed39ac86
Tony Li <84836605+Pevulz@users.noreply.github.com>
2022-09-29 16:05:47 -0400

    Add files via upload

---
+++ CISC4900Project-main/Quiz/Quiz.js
@@ -0,0 +1,10 @@
+let countJava = 0;
+let countPython = 0;
+let countC = 0;
+let countJS = 0;
+let countR = 0;
+let countSQL = 0;
+let countGo = 0;
+let countCSharp = 0;
+
+const check = document.querySelector("checkmark")
