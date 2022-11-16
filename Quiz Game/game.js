dedd9eb2fa1e032fbafd72140303b3cbd3c1f41f
SafiyyahKazim <safiyyahkazim@gmail.com>
2022-11-16 17:21:59 -0500

    end page

--- Quiz Game/game.js
+++ Quiz Game/game.js
@@ -59,7 +59,7 @@ getNewQuestion= () => {
     if(availableQuestions.length===0||questionCounter>MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore',score)
     
-    return window.location.assign('/end.html')//keeps track of score
+    return window.location.assign('end.html')//keeps track of score
 }
    questionCounter++
    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}` 
