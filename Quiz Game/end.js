019aa6e3691bdb330804c3cdfb84630431904a7d
SafiyyahKazim <safiyyahkazim@gmail.com>
2022-12-05 15:09:45 -0500

    working on end page

--- Quiz Game/end.js
+++ Quiz Game/end.js
@@ -3,4 +3,31 @@
  const finalScore= document.querySelector('#finalScore')
  const mostRecentScore= document.querySelector('#mostRecentScore')
 
- 
+ const highScores= JSON.parse(localStorage.getItem('highScore')) || []
+
+ const MAX_HIGH_SCORE=5
+
+ username.addEventListener('keyup',()=>{
+    saveScoreBtn.disabled = !username.value
+ })
+
+ saveHighScore = e =>{
+    e.preventDefault()
+
+    const score = {
+        score:mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores.sort((a,b) => {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores',JSON.stringify(highScores))
+    window.location.assign('/')
+    
+ }
