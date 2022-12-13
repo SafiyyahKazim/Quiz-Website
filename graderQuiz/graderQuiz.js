a1db7ef81482d9c6a02641183848ffcf47f8213f
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2022-12-13 14:37:45 -0500

    completed graderquiz

--- graderQuiz/graderQuiz.js
+++ graderQuiz/graderQuiz.js
@@ -18,25 +18,50 @@ function starting() {
     //---------------------------------------------------------------Grade 3-----------------------------------------------------------
     if(gradechoice == 3) {
         document.querySelector('.contentbox').style.display = 'none'
+        document.querySelector('.container').style.backgroundImage = "url('background/hello-third-grade-background-inspirational-quotes-typography-lettering-design-first-day-school_557783-209.webp')";
         const graderQuiz = [
             {
-                question: "What is 1 + 3?",
-                a: "3",
-                b: "2",
-                c: "10",
-                d: "1",
+                question: "What is 10 x 3?",
+                a: "100",
+                b: "30",
+                c: "1000",
+                d: "11,000",
                 correct: "b",
             },
             {
-                question: "What is 1 + 1?",
-                a: "32",
-                b: "42",
-                c: "10",
-                d: "2",
-                correct: "d",
+                question: "What is the largest planet in the solar system?",
+                a: "Saturn",
+                b: "Jupiter",
+                c: "Mercury",
+                d: "Earth",
+                correct: "b",
+            }, 
+            {
+                question: "Which animal is the fastest land animal?",
+                a: "Lion",
+                b: "Tiger",
+                c: "Cheetah",
+                d: "Panda",
+                correct: "c",
+            }, 
+            {
+                question: "12 - 6 = ?",
+                a: "6",
+                b: "5",
+                c: "7",
+                d: "10",
+                correct: "a",
+            }, 
+            {
+                question: "How much hour is one day on Earth?",
+                a: "24 hours",
+                b: "12 hours",
+                c: "48 hours",
+                d: "25 hours",
+                correct: "a",
             },  
         ];
-        // Start of the program
+
         loadQuestion()
 
         // Loads the question
@@ -97,25 +122,50 @@ function starting() {
 //---------------------------------------------------------------Grade 4-----------------------------------------------------------
     if(gradechoice == 4) {
         document.querySelector('.contentbox').style.display = 'none';
+        document.querySelector('.container').style.backgroundImage = "url('background/hello-fourth-grade-background-inspirational-quotes-typography-lettering-design-first-day-school_557783-200.webp')";
         const graderQuiz = [
             {
-                question: "What is 1 + 4?",
-                a: "3",
-                b: "2",
-                c: "10",
-                d: "1",
-                correct: "b",
+                question: "How many days does it take for the Earth to go around the sun 360 degree?",
+                a: "365 days",
+                b: "2 years",
+                c: "100 days",
+                d: "1 day",
+                correct: "a",
             },
             {
-                question: "What is 1 + 1?",
-                a: "32",
-                b: "42",
-                c: "10",
-                d: "2",
+                question: "What is the force that keeps us on Earth?",
+                a: "Weight",
+                b: "Oxygen",
+                c: "Electricity",
+                d: "Gravity",
                 correct: "d",
             },
+            {
+                question: "In what state of matter is water when it is frozen into ice?",
+                a: "Gas",
+                b: "Energy",
+                c: "Liquid",
+                d: "Solid",
+                correct: "d",
+            },
+            {
+                question: "What is (14 + 5) - (5 - 2)?",
+                a: "16",
+                b: "13",
+                c: "12",
+                d: "23",
+                correct: "a",
+            },
+            {
+                question: "9 x n = 45. What is n?",
+                a: "6",
+                b: "3",
+                c: "5",
+                d: "7",
+                correct: "c",
+            },
         ];
-        // Start of the program
+
         loadQuestion()
 
         // Loads the question
@@ -174,25 +224,50 @@ function starting() {
 //---------------------------------------------------------------Grade 5-----------------------------------------------------------
     if(gradechoice == 5) {
         document.querySelector('.contentbox').style.display = 'none';
+        document.querySelector('.container').style.backgroundImage = "url('background/hello-fifth-grade-background-inspirational-quotes-typography-lettering-design-first-day-school_557783-197.webp')";
         const graderQuiz = [
             {
-                question: "What is 1 + 5?",
-                a: "3",
-                b: "2",
-                c: "10",
-                d: "1",
-                correct: "b",
+                question: "What unit of measurement is abbreviated oz?",
+                a: "Ounces",
+                b: "Pounds",
+                c: "Liters",
+                d: "Gallon",
+                correct: "a",
             },
             {
-                question: "What is 1 + 1?",
-                a: "32",
-                b: "42",
+                question: "Which continent is the least populated?",
+                a: "Asia",
+                b: "Antarctica",
+                c: "Australia",
+                d: "Europe",
+                correct: "b",
+            },   
+            {
+                question: "Which planet in our solar system is known for its beautiful ring?",
+                a: "Mars",
+                b: "Saturn",
+                c: "Jupiter",
+                d: "Earth",
+                correct: "b",
+            },   
+            {
+                question: "How many sides does a quadrangle have?",
+                a: "3",
+                b: "5",
                 c: "10",
-                d: "2",
+                d: "4",
                 correct: "d",
             },   
+            {
+                question: "Numbers: 42, 23, 1, 25, 454, which number is the medium?",
+                a: "25",
+                b: "1",
+                c: "23",
+                d: "42",
+                correct: "a",
+            },   
         ];
-        // Start of the program
+
         loadQuestion()
 
         // Loads the question
@@ -264,47 +339,5 @@ function starting() {
 
 
 
-
-
-
-
-
-
-
-// Holds the question and answers
-/*const careerQuiz = [
-    {
-        question: "What is 1 + 1?",
-        a: "3",
-        b: "2",
-        c: "10",
-        d: "1",
-        correct: "b",
-    },
-    {
-        question: "What is 1 + 1?",
-        a: "32",
-        b: "42",
-        c: "10",
-        d: "2",
-        correct: "d",
-    },
-    
-    
-];*/
-
-const quiz = document.getElementById('quiz')
-const question = document.getElementById('question')
-const answereh = document.querySelectorAll('.answer')
-const texta = document.getElementById('texta')
-const textb = document.getElementById('textb')
-const textc = document.getElementById('textc')
-const textd = document.getElementById('textd')
-const submitButton = document.getElementById('submit')
-
-// set variable
-let currentQuestion = 0
-let score = 0
-let percent = 0
 
 
