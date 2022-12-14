e6a0883e63d3c4f7ea6337cb49c87713ce7ba846
SafiyyahKazim <safiyyahkazim@gmail.com>
2022-12-13 22:21:02 -0500

    safiyyah kazim

--- Quiz Game/highscores.js
+++ Quiz Game/highscores.js
@@ -1,8 +1,11 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores=JSON.parse(localStorage.getItem('highScores')) ||[]
+//highscores for an empty array
 
 
 highScoresList.innerHTML=
 highScores.map(score =>{
     return`<li class="high-score">${score.name}-${score.score}</li>`
 }).join('')
+//iteratoring through new array 
+//score that you see and what ever you type in at the end of the quiz it will fill in 
